0ebee9f8c12f7666266f6549b9d0f4811465db51
hungp03 <ph.hung0903@gmail.com>
2023-11-05 18:28:57 +0700

    1st commit

---
+++ target/classes/app/storemanagement/StoreManagement.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package app.storemanagement;
+
+/**
+ *
+ * @author Hung Pham
+ */
+public class StoreManagement {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
