12a51f7d5da0f189f74602712a93b98c1160b7d9
hungp03 <ph.hung0903@gmail.com>
2023-11-21 09:32:29 +0700

    add dll file

--- src/main/java/app/storemanagement/model/Connection/DBConnection.java
+++ src/main/java/app/storemanagement/model/Connection/DBConnection.java
@@ -1,9 +1,9 @@
 package app.storemanagement.model.Connection;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+//import java.util.Properties;
 
 public class DBConnection {
 
@@ -21,3 +21,26 @@ public class DBConnection {
         }
     }
 }
+
+/*public class DBConnection {
+
+    private static final String DATABASE_URL = "jdbc:sqlserver://localhost";
+    private static final String DATABASE_NAME = "StoreManagementDb";
+ 
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            Properties connectionProperties = new Properties();
+            connectionProperties.setProperty("integratedSecurity", "true");
+            connectionProperties.setProperty("encrypt", "true");
+            connectionProperties.setProperty("trustServerCertificate", "true");
+
+            connection = DriverManager.getConnection(DATABASE_URL + ";databaseName=" + DATABASE_NAME, connectionProperties);
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return connection;
+    }
+}*/
--- target/classes/app/storemanagement/model/Connection/DBConnection.java
+++ target/classes/app/storemanagement/model/Connection/DBConnection.java
@@ -1,23 +1,46 @@
 package app.storemanagement.model.Connection;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
+
+//public class DBConnection {
+//
+//    private static final String URL = "jdbc:sqlserver://127.0.0.1;databaseName=StoreManagementDb";
+//    private static final String USERNAME = System.getenv("DB_USER1");
+//    private static final String PASSWORD = System.getenv("DB_PASSWORD1");
+//
+//    public static Connection getConnection() {
+//        try {
+//            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//            return DriverManager.getConnection(URL + ";user=" + USERNAME + ";password=" + PASSWORD + ";integratedSecurity=true;encrypt=true;trustServerCertificate=true");
+//        } catch (ClassNotFoundException | SQLException e) {
+//            System.out.println(e.getMessage());
+//            return null;
+//        }
+//    }
+//}
 
 public class DBConnection {
 
-    private static final String URL = "jdbc:sqlserver://127.0.0.1;databaseName=StoreManagementDb";
-    private static final String USERNAME = System.getenv("DB_USER1");
-    private static final String PASSWORD = System.getenv("DB_PASSWORD1");
+    private static final String DATABASE_URL = "jdbc:sqlserver://localhost";
+    private static final String DATABASE_NAME = "StoreManagementDb";
+ 
 
     public static Connection getConnection() {
+        Connection connection = null;
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            return DriverManager.getConnection(URL + ";user=" + USERNAME + ";password=" + PASSWORD + ";integratedSecurity=true;encrypt=true;trustServerCertificate=true");
+            Properties connectionProperties = new Properties();
+            connectionProperties.setProperty("integratedSecurity", "true");
+            connectionProperties.setProperty("encrypt", "true");
+            connectionProperties.setProperty("trustServerCertificate", "true");
+
+            connection = DriverManager.getConnection(DATABASE_URL + ";databaseName=" + DATABASE_NAME, connectionProperties);
         } catch (ClassNotFoundException | SQLException e) {
             System.out.println(e.getMessage());
-            return null;
         }
+        return connection;
     }
 }
